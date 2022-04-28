f73f2626a47945e67b5e6f8587ca0eea941c847d
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-04-28 13:26:07 +0700

    edit kode PenggunaController

--- src/main/java/com/controller/PenggunaController.java
+++ src/main/java/com/controller/PenggunaController.java
@@ -1,9 +1,47 @@
 package com.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.model.dto.PenggunaDto;
+import com.model.entity.Pengguna;
+import com.repository.PenggunaRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/pengguna")
 public class PenggunaController {
+
+    private final PenggunaRepository penggunaRepository;
+    public PenggunaController(PenggunaRepository penggunaRepository) {
+        this.penggunaRepository = penggunaRepository;
+    }
+
+    @GetMapping
+    public List<PenggunaDto> getPengguna(){
+        List<Pengguna> listPengguna = penggunaRepository.findAll();
+        return listPengguna.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+    @PostMapping
+    public PenggunaDto insert(@RequestBody PenggunaDto dto){
+        Pengguna pengguna = convertToEntity(dto);
+        penggunaRepository.save((pengguna));
+        return dto;
+    }
+    private PenggunaDto convertToDto(Pengguna pengguna){
+        PenggunaDto penggunaDto = new PenggunaDto();
+        penggunaDto.setKode(pengguna.getKodePengguna());
+        penggunaDto.setNama(pengguna.getNamaPengguna());
+        penggunaDto.setPeran(pengguna.getPeranPengguna());
+        penggunaDto.setAlamat(pengguna.getAlamat());
+        return penggunaDto;
+    }
+    private Pengguna convertToEntity(PenggunaDto dto){
+        Pengguna pengguna = new Pengguna();
+        pengguna.setKodePengguna(dto.getKode());
+        pengguna.setNamaPengguna(dto.getNama());
+        pengguna.setPeranPengguna(dto.getPeran());
+        pengguna.setAlamat(dto.getAlamat());
+        return pengguna;
+    }
 }
